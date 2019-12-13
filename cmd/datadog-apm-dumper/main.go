f6477154942321598d7df71dbb835a6fb316da55
Shoichi Kaji <skaji@cpan.org>
2019-12-13 18:31:48 +0900

    support statsd

--- cmd/datadog-apm-dumper/main.go
+++ cmd/datadog-apm-dumper/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
 
 	"github.com/vmihailenco/msgpack"
 )
@@ -24,7 +32,9 @@ type Span struct {
 	Error    int32              `json:"error"`
 }
 
-func main() {
+func apmServer(ctx context.Context, addr string) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		var spans [][]Span
 		if err := msgpack.NewDecoder(r.Body).UseJSONTag(true).Decode(&spans); err != nil {
@@ -32,10 +42,86 @@ func main() {
 			w.WriteHeader(400)
 			return
 		}
+		fmt.Println("\033[1;31mapm\033[m | ---", r.RemoteAddr)
 		b, _ := json.MarshalIndent(spans, "", "  ")
-		fmt.Println(string(b))
+		for _, line := range strings.Split(string(b), "\n") {
+			if len(line) > 0 {
+				fmt.Println("\033[1;31mapm\033[m |", line)
+			}
+		}
 		w.WriteHeader(200)
 	}
-	fmt.Println("Accepting connections at http://localhost:8126/")
-	http.ListenAndServe("localhost:8126", http.HandlerFunc(handler))
+	svc := &http.Server{Addr: addr, Handler: http.HandlerFunc(handler)}
+	done := make(chan error, 1)
+	go func() {
+		<-ctx.Done()
+		if err := svc.Shutdown(context.Background()); err != nil {
+			done <- err
+		}
+		close(done)
+	}()
+	if err := svc.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		cancel()
+		<-done
+		return err
+	}
+	return <-done
+}
+
+func statsdServer(ctx context.Context, addr string) error {
+	conn, err := net.ListenPacket("udp", addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	go func() {
+		<-ctx.Done()
+		conn.Close()
+	}()
+
+	var buf [1500]byte
+	for {
+		n, addr, err := conn.ReadFrom(buf[:])
+		if err != nil {
+			if ctx.Err() != nil {
+				return nil
+			}
+			return err
+		}
+		fmt.Println("\033[1;34mstat\033[m| ---", addr)
+		for _, line := range strings.Split(string(buf[:n]), "\n") {
+			if len(line) > 0 {
+				fmt.Println("\033[1;34mstat\033[m|", line)
+			}
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+		<-sig
+		cancel()
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		fmt.Println("start apm server at http://localhost:8126")
+		if err := apmServer(ctx, "localhost:8126"); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		fmt.Println("start statsd server at udp://localhost:8125")
+		if err := statsdServer(ctx, "localhost:8125"); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	wg.Wait()
+	fmt.Println("finish")
 }
